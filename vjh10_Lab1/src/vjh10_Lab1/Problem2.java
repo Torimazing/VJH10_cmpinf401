fcc4988452900c995366f58d5b35f8bf01411d63
VJH10_cmpinf401 <112402710+Torimazing@users.noreply.github.com>
2022-09-10 19:20:10 -0400

    Problem 2 Complete

--- vjh10_Lab1/src/vjh10_Lab1/Problem2.java
+++ vjh10_Lab1/src/vjh10_Lab1/Problem2.java
@@ -1,5 +1,15 @@
 package vjh10_Lab1;
 
+import javax.swing.JOptionPane;
+
 public class Problem2 {
+	public static void main(String[] args) {
+		String radius = JOptionPane.showInputDialog("Please input a radius");
+		int numR = Integer.parseInt(radius);
+		double area = (Math.PI * Math.pow(numR, 2));
+		double perim = (Math.PI * 2 * numR);
+		JOptionPane.showMessageDialog(null, "The circle with radius " + radius + 
+				" has an area of " + area + " and a perimeter of " + perim);
+	}
 
 }
