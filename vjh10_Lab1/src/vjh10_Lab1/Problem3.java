1b776ce6c11c01c1af014fbc79de30818cf34ba1
VJH10_cmpinf401 <112402710+Torimazing@users.noreply.github.com>
2022-09-10 19:35:10 -0400

    Finished Part 3

--- vjh10_Lab1/src/vjh10_Lab1/Problem3.java
+++ vjh10_Lab1/src/vjh10_Lab1/Problem3.java
@@ -1,5 +1,18 @@
 package vjh10_Lab1;
 
-public class Problem3 {
+import javax.swing.JOptionPane;
 
+public class Problem3 {
+	public static void main(String[] args) {
+		String money = JOptionPane.showInputDialog("Please input a dollar amount between 1 and 9999");
+		int nummun = Integer.parseInt(money);
+		int grands = nummun / 1000;
+		nummun = nummun - (grands * 1000);
+		int ben = nummun / 100;
+		nummun = nummun - (ben*100);
+		int saw = nummun / 10;
+		int bucks = nummun - (saw*10);
+		JOptionPane.showMessageDialog(null, money + " is equivelant to: \n" + grands + " Grands\n" + 
+		ben + " Benjamins \n" + saw + " Sawbucks \n" + bucks + " Bucks");
+	}
 }
