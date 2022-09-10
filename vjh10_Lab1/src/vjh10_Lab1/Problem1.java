ae4784b850c225801d4b58130d7ca1b40e820aa6
VJH10_cmpinf401 <112402710+Torimazing@users.noreply.github.com>
2022-09-10 17:10:23 -0400

    Finished Problem 1

--- vjh10_Lab1/src/vjh10_Lab1/Problem1.java
+++ vjh10_Lab1/src/vjh10_Lab1/Problem1.java
@@ -1,5 +1,17 @@
 package vjh10_Lab1;
 
-public class Problem1 {
+import javax.swing.JOptionPane;
 
+public class Problem1 {
+	public static void main(String[] args) {
+		String sideA = JOptionPane.showInputDialog("Please enter a number for Side A");
+		String sideB = JOptionPane.showInputDialog("Please enter a number for Side B");
+		
+		int numA = Integer.parseInt(sideA);	
+		int numB = Integer.parseInt(sideB);
+		double sideC = Math.sqrt(Math.pow(numA, 2) + Math.pow(numB, 2));
+		JOptionPane.showMessageDialog(null, "The Hypotenuse is " + sideC);	
+	
+
+	}
 }
