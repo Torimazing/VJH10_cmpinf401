212b8749461c7997ceee128ab55f14859678af1a
VJH10_cmpinf401 <112402710+Torimazing@users.noreply.github.com>
2022-09-18 20:58:21 -0400

    Finished Lab 3

--- Lab 3/src/UnitConverter.java
+++ Lab 3/src/UnitConverter.java
@@ -2,8 +2,8 @@ import javax.swing.JOptionPane;
 
 public class UnitConverter {
 	public static void main(String[] args) {
-		String measure = "";
-		String unit = "";
+		String measure = null;
+		String unit = null;
 		String userInput;
 		String workUnit = "";
 		Float newNumber = null;
@@ -13,7 +13,7 @@ public class UnitConverter {
 			userInput = JOptionPane.showInputDialog("Please enter a number");
 			measure = userInput.substring(0, userInput.indexOf(" "));
 			unit = userInput.substring(userInput.indexOf(" ")+1, userInput.length());
-		}while (measure == "" || unit == "");
+		}while (measure == null || unit == null);
 		
 		//cleanup input mostly derived from class examples
 		float numMeas = Float.parseFloat(measure);
@@ -24,30 +24,29 @@ public class UnitConverter {
 		}
 		workUnit = workUnit.toUpperCase();
 		
-		//evaluate variables
-		if (workUnit == "CM") {
+		//evaluate variables (using equals method from book)
+		if (workUnit.equals("CM")) {
 			newNumber = (float) (numMeas / 2.54);
 			newUnit = "IN";
-			System.out.print("Bitch");
-		}else if (workUnit == "IN") {
+		}else if (workUnit.equals("IN")) {
 			newNumber = (float) (numMeas * 2.54);
 			newUnit = "CM";
-		}else if (workUnit == "YD") {
+		}else if (workUnit.equals("YD")) {
 			newNumber = (float) (numMeas / 1.09361);
 			newUnit = "M";
-		}else if (workUnit == "M") {
-			newNumber = (float) (numMeas * 0.9144);
+		}else if (workUnit.equals("M")) {
+			newNumber = (float) (numMeas / 0.9144);
 			newUnit = "YD";
-		}else if (workUnit == "G" || workUnit == "GM") {
+		}else if (workUnit.equals("G") || workUnit.equals("GM")) {
 			newNumber = (float) (numMeas / 28.35);
 			newUnit = "OZ";
-		}else if (workUnit == "OZ") {
-			newNumber = (float) (numMeas *0.035274);
+		}else if (workUnit.equals("OZ")) {
+			newNumber = (float) (numMeas / 0.035274);
 			newUnit = "G";
-		}else if (workUnit == "LB") {
+		}else if (workUnit.equals("LB")) {
 			newNumber = (float) (numMeas / 2.20462);
 			newUnit = "KG";
-		}else if (workUnit == "KG") {
+		}else if (workUnit.equals("KG")) {
 			newNumber = (float) (numMeas / 0.453592);
 			newUnit = "LB";
 		}
